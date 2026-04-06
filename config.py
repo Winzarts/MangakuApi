1f111c3421f0b767dd011b271ca065652cf7811e
Bluenight <91639047+Winzarts@users.noreply.github.com>
2026-04-06 22:33:52 +0700

    Add Accept-Language and Connection headers

--- config.py
+++ config.py
@@ -11,5 +11,7 @@ HEADERS = {
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
             "AppleWebKit/537.36 (KHTML, like Gecko) "
             "Chrome/120.0.0.0 Safari/537.36"
-        )
+        ),
+    "Accept-Language": "en-US,en;q=0.9",
+    "Connection": "keep-alive"
     }
